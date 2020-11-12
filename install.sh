1f23872f2e5a2b4bc08ca7b61e86fc105029ddc4
Atio Cre <48242727+enxier@users.noreply.github.com>
2020-11-12 21:15:56 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ install_soga() {
     fi
 
     if  [ $# == 0 ] ;then
-        last_version=$(curl -Ls "https://api.github.com/repos/RManLuo/crack-soga-v2ray/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/enxier/crack-soga/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}检测 soga 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 soga 版本安装${plain}"
             exit 1
